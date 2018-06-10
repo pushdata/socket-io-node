e8ec71651feefac03ad40e7381fd8215db995f41
Sai Prudhvi <schode1@binghamton.edu>
2018-06-10 11:26:32 -0500

    Added HTML Form and showing incoming messages

--- public/js/index.js
+++ public/js/index.js
@@ -1,19 +1,27 @@
 var socket = io();
+
 socket.on('connect', function() {
     console.log('Connected to server');
 });
 
 socket.on('newMessage', function(messageData) {
     console.log(messageData);
+    var message = jQuery('<li></li>');
+    message.text(`${messageData.from} : ${messageData.text} ${new Date().toLocaleTimeString()}`);
+    jQuery('#messages').append(message);
 })
 
 socket.on('disconnect', function() {
     console.log('unable to connect to server!');
 })
 
-socket.emit('createMessage', {
-    from: 'Sai',
-    text: 'Just a sample text'
-}, function(ack) {
-    console.log(ack);
+
+jQuery('#message-form').on('submit', (e) => {
+    e.preventDefault();
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[name=message]').val()
+    }, function(ack) {
+        console.log(ack);
+    })
 })
